426561611ce0622aa8de5e24cf2c0774afc4c791
Guillaume Bailly <guillaume.gbailly@gmail.com>
2021-02-24 06:14:33 +0100

    reverse-string (clojure)

--- clojure/reverse-string/src/reverse_string.clj
+++ clojure/reverse-string/src/reverse_string.clj
@@ -1,9 +1,5 @@
 (ns reverse-string
     (:use [clojure.string :only (blank?)]))
 
-(defn reverse-string
-  ([s] (reverse-string s ""))
-  ([s acc]
-   (if (blank? s)
-     acc
-     (recur (subs s 1 (count s)) (str (subs s 0 1) acc)))))
+(defn reverse-string [s]
+  (apply str (into () (char-array s))))
