d9acbc4501e473d8366b067389bbb70a712a8832
Guillaume Bailly <guillaume.gbailly@gmail.com>
2021-02-26 00:06:58 +0100

    matching-brackets (scala)

--- scala/matching-brackets/src/main/scala/MatchingBrackets.scala
+++ scala/matching-brackets/src/main/scala/MatchingBrackets.scala
@@ -4,13 +4,14 @@ object MatchingBrackets {
   def isPaired(s: String): Boolean = {
     def isPaired(s: List[Char], acc: List[Char]): Boolean = s match {
       case List() => acc.isEmpty
-      case head :: tail => head match {
-        case '[' | '{' | '(' => isPaired(tail, acc.+:(head))
-        case ']' | '}' | ')' => if (acc.isEmpty || acc.head != mappings.get(head).get) false else isPaired(tail, acc.tail)
-        case _ => isPaired(s.tail, acc)
-      }
+      case head :: tail =>
+        if (mappings.values.exists(_ == head)) isPaired(tail, head :: acc)
+        else if (mappings.contains(head))
+          if (acc.isEmpty || acc.head != mappings(head)) false
+          else isPaired(tail, acc.tail)
+        else isPaired(s.tail, acc)
     }
 
-    isPaired(s.toCharArray.toList, List())
+    isPaired(s.toList, List())
   }
 }
